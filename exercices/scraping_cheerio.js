4878f86321cf95df3e31494dfc28188eca6d0a61
Andrea Brandani <andreabrandani@MacBookProAndreanefer.local>
2020-04-24 11:22:50 +0200

    exercice du jour pour ce matin

--- exercices/scraping_cheerio.js
+++ exercices/scraping_cheerio.js
@@ -5,6 +5,10 @@ const $ = cheerio.load(page)
 const row = $('.row')
 const parts = $('.col-sm-4.col-lg-4.col-md-4', row)
 parts.each((index, part) => {
-  // le prix est le texte du premier <h4>
-  // le nom est l'attribut "title" de l'élément <a> à l'intérieur du deuxième <h4>
+    if (index !== 0) {
+        result.push({
+            nom: $('body > div.wrapper > div.container.test-site > div > div.col-md-9 > div > div : nth-child(4) > div > div.caption > h4 : nth-child(2) > a', h4).attr('title'),
+            prix: $('body > div.wrapper > div.container.test-site > div > div.col-md-9 > div > div : nth-child(4) > div > div.caption > h4.pull-right.price', h4).text()
+        })
+    }
 })
