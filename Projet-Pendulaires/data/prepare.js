a133b9e3028057b4e4915ff56122ebcae74c2ab1
Andrea Brandani <andreabrandani@MacBookProAndreanefer.local>
2020-05-07 15:51:16 +0200

    Rajoue du ficher data.json et de prepare.js

---
+++ Projet-Pendulaires/data/prepare.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+
+const passagersCsv = fs.readFileSync('NombrePassagersTransportes.csv', 'utf-8')
+const kmsCsv = fs.readFileSync('NombreKMParcourus.csv', 'utf-8')
+
+const removeSemiColons = d =>
+  Array.from(d).filter(letter => letter !== ';').join('')
+
+const fixLine = line => line.trim().split(',')
+  .map(removeSemiColons)
+  .map(d => isNaN(Number(d)) ? 0 : Number(d))
+
+const [ kmHead, ...kmLines] = kmsCsv.split('\n').map(fixLine)
+const [ pHead, ...pLines ] = passagersCsv.split('\n')
+  .map(line => line.split(',').map(d => isNaN(Number(d)) ? 0 : Number(d)))
+
+const ajouterCles = prefix => d => ({
+  annee: d[0],
+  [`${prefix}_train`]: d[1],
+  [`${prefix}_tpr`]: d[2],
+  [`${prefix}_bateau`]: d[3],
+})
+
+const passagers = pLines.map(ajouterCles('passagers'))
+const km = kmLines.map(ajouterCles('km'))
+
+
+const chercherValeursKm = annee => km.find(d => d.annee === annee)
+
+const resultat = passagers.map(d => ({ ...d, ...chercherValeursKm(d.annee) }))
+
+console.log(
+  JSON.stringify(resultat, null, 2)
+)
